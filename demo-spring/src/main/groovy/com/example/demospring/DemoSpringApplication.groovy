d2aff8b15939659b85dfc8aa4df0364c06214467
SANGMOON <sangmoon1113@gmail.com>
2023-12-02 22:45:17 +0900

    init

---
+++ demo-spring/src/main/groovy/com/example/demospring/DemoSpringApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.demospring
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DemoSpringApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(DemoSpringApplication, args)
+    }
+
+}
