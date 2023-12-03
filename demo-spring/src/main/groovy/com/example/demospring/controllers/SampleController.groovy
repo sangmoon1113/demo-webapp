e04d87503e26d7e31c9f5a5fb765a531b342762c
SANGMOON <sangmoon1113@gmail.com>
2023-12-03 15:23:01 +0900

    feat: edit api

--- demo-spring/src/main/groovy/com/example/demospring/controllers/SampleController.groovy
+++ demo-spring/src/main/groovy/com/example/demospring/controllers/SampleController.groovy
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api")
 class SampleController {
     @RequestMapping(value="/hello", method = RequestMethod.GET)
-    public String getHello() {
-        return "Hello World";
+    String getHello() {
+        return "Hello World"
     }
 }
